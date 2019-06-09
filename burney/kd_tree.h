ed136710265edc32231fac578d482bee1c68f61b
fsps60312 <fsps60312@yahoo.com.tw>
2019-06-09 14:45:57 +0800

    kd tree query

--- burney/kd_tree.h
+++ burney/kd_tree.h
@@ -31,10 +31,51 @@ struct Node{
 	DataPoint data;
 	bool is_leaf()const{return l==NULL&&r==NULL;}
 };
-struct KD_tree{
+class KD_tree{
+  public:
+	void build_tree(const vector<DataPoint>&data){
+		vector<DataPoint>data_copy;
+		data_copy.assign(data.begin(),data.end());
+		if(!data_copy.empty())build_tree(root,data_copy,0);
+	}
+	bool query_nearst(const Point &target,DataPoint &result)const{
+		if(root==NULL)return false;
+		query_nearst(root,target,result,false);
+		return true;
+	}
+  private:
 	Node *root=NULL;
+	double distance(const Point &a,const Point &b)const{
+		double ans=0;
+		for(int i=0;i<NUM_DIMS;i++)ans+=(a.x[i]-b.x[i])*(a.x[i]-b.x[i]);
+		return ans;
+	}
+	double distance(const Point &a,const Box &b)const{
+		double ans=0;
+		for(int i=0;i<NUM_DIMS;i++){
+			double d=0;
+			if(a.x[i]<b.mn.x[i])d=b.mn.x[i]-a.x[i];
+			if(a.x[i]>b.mx.x[i])d=a.x[i]-b.mx.x[i];
+			ans+=d*d;
+		}
+		return ans;
+	}
+	void query_nearst(Node const *o,const Point &target,DataPoint &result,bool result_assigned)const{
+		assert(o!=NULL);
+		if(result_assigned&&distance(target,result)<=distance(target,o->box))return;
+		if(o->is_leaf()){result=o->data;return;}
+		int split_dim=o->split_dim;
+		double split_value=o->split_value;
+		if(target.x[split_dim]<split_value){
+			query_nearst(o->l,target,result,result_assigned);
+			query_nearst(o->r,target,result,true);
+		}else{
+			query_nearst(o->r,target,result,result_assigned);
+			query_nearst(o->l,target,result,true);
+		}
+	}
 	void build_tree(Node* &o,vector<DataPoint>data,int split_dim){
-		if(data.empty()){o=NULL;return;}
+		assert(!data.empty());
 		o=new Node();
 
 		// leaf: assign data
